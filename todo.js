5db41f75ad611a9fb626bcfb7a9837fe293c7a57
AkhiSharmin <akhisharmin100@gmail.com>
2024-02-16 21:36:23 +0600

    added task edit

--- todo.js
+++ todo.js
@@ -31,11 +31,41 @@ window.addEventListener('load', () => {
         task_content_el.appendChild(task_input_el);
 
 
-        const task_action_el = document.createElement('div');
-        task_action_el.classList.add('actions')
+        const task_actions_el = document.createElement('div');
+        task_actions_el.classList.add('actions');
 
+        const task_edit_el = document.createElement('div');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerHTML = 'Edit';
+
+
+        const task_delete_el = document.createElement('div');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerHTML = 'Delete';
+
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el)
 
         list_el.appendChild(task_el);
+
+        input.value = "";
+
+
+        // edit 
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerText.toLocaleLowerCase() == 'edit') {
+                task_input_el.removeAttribute('readonly');
+                task_input_el.focus();
+                task_edit_el.innerText = 'save';
+            } else {
+                // console.log('Save');
+                task_input_el.setAttribute('readonly', 'readonly');
+                task_edit_el.innerText = 'Edit';
+            }
+        })
 
     })
 })
